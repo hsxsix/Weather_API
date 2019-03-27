4e7431af6b4d60e7dc46337708f22b5a5fb4d5f4
hsxsix <hsxsix@gmail.com>
2019-03-27 18:03:40 +0800

    in development...

---
+++ searchweather.py
@@ -0,0 +1,186 @@
+#encoding:utf-8
+import datetime
+from urllib.parse import quote 
+
+import requests
+from lxml import etree as et 
+
+class GetWeather(): 
+    def __init__(self):
+        baiduhomepage = "https://www.baidu.com"
+        self.url = (baiduhomepage+"/s?ie=utf-8"
+                "&f=8"
+                "&rsv_bp=0"
+                "&rsv_idx=1"
+                "&tn=baidu"
+                "&wd=%s"
+                "&rqlang=cn"
+                "&rsv_enter=1"
+                "&rsv_sug3=2")
+        self.headers = {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "zh-CN,zh;q=0.9",
+                "Cache-Control": "max-age=0",
+                "Connection": "keep-alive",
+                "Host": "www.baidu.com",
+                "Cookie":"BAIDUID=05D73F0269B5857EEBD199CE0F4F9D67:FG=1",
+                "Upgrade-Insecure-Requests": "1",
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.12 Safari/537.36"
+                }
+
+    def get_weather_data(self, keywords, node=None):
+        kd = (quote(keywords+" 天气",'utf-8'))
+        html_code = requests.get(url=self.url % kd, headers=self.headers, timeout=5)
+        # print(html_code.text)
+        selector = et.HTML(html_code.content, parser=et.HTMLParser(encoding='utf-8'))
+        weather_selector = selector.xpath('//div[@id="content_left"]//div[@id="1"]')
+        if weather_selector:
+            try:
+                weather_data = self.parser_weather_data(weather_selector[0], node)
+                return weather_data 
+            except Exception as e:
+                print(e)
+                return {"code":"error", "city":keywords, "data":"获取数据失败!Get data failed!"}
+        else:
+            return {"code":"error", "city":keywords, "data":"获取数据失败!Get data failed!"}
+        
+    def parser_weather_data(self, weather_selector, node):
+        city = ''.join(weather_selector.xpath('.//h3[@class="t c-gap-bottom-small"]/a//text()'))[:-17]
+        today = weather_selector.xpath('.//div[@class="op_weather4_twoicon"]/a[1]')[0]
+        today_date = ''.join(today.xpath('./p[@class="op_weather4_twoicon_date"]/text()')).strip() 
+        today_icon = ''.join(today.xpath('.//div[@class="op_weather4_twoicon_icon"]/@style')).split(";")[0].split("url(")[1].replace(")",'')
+        current_temp = ''.join(today.xpath('.//span[@class="op_weather4_twoicon_shishi_title"]/text()')).strip()  
+        current_weather = ''.join(today.xpath('.//span[@class="op_weather4_twoicon_shishi_data"]/i/text()')).strip('℃')
+        today_temp = ''.join(today.xpath('.//p[@class="op_weather4_twoicon_temp"]/text()')).strip('℃')
+        today_weather = ''.join(today.xpath('.//p[@class="op_weather4_twoicon_weath"]/text()')).strip() #.replace(' ','').replace('\n','')
+        today_wind = ''.join(today.xpath('.//p[@class="op_weather4_twoicon_wind"]/text()'))
+        today_aqi = ','.join(today.xpath('.//div[@class="op_weather4_twoicon_realtime_quality_wrap"]/span/span/text()')).replace('\n','').replace(' ','')
+
+        tomorrow = weather_selector.xpath('.//div[@class="op_weather4_twoicon"]/a[2]')[0]
+        tomorrow_date = ''.join(tomorrow.xpath('.//p[@class="op_weather4_twoicon_date_day"]/text()'))
+        tomorrow_week = ''.join(tomorrow.xpath('.//p[@class="op_weather4_twoicon_date"]/text()')).strip() 
+        tomorrow_icon = ''.join(tomorrow.xpath('.//div[@class="op_weather4_twoicon_icon"]/@style')).split(";")[0].split("url(")[1].replace(")",'')
+        tomorrow_temp = ''.join(tomorrow.xpath('.//p[@class="op_weather4_twoicon_temp"]/text()')).strip().strip('℃')
+        tomorrow_weather = ''.join(tomorrow.xpath('.//p[@class="op_weather4_twoicon_weath"]/text()')).strip() 
+        tomorrow_wind = ''.join(tomorrow.xpath('.//p[@class="op_weather4_twoicon_wind"]/text()'))
+        tomorrow_aqi = ''.join(tomorrow.xpath('.//div[@class="op_weather4_twoicon_realtime_quality_wrap"]/span/text()')).strip() 
+        
+        acquired = weather_selector.xpath('.//div[@class="op_weather4_twoicon"]/a[3]')[0]
+        after_tomorrow_date = ''.join(acquired.xpath('.//p[@class="op_weather4_twoicon_date_day"]/text()'))
+        after_tomorrow_week = ''.join(acquired.xpath('.//p[@class="op_weather4_twoicon_date"]/text()')).strip()  
+        after_tomorrow_icon = ''.join(acquired.xpath('.//div[@class="op_weather4_twoicon_icon"]/@style')).split(";")[0].split("url(")[1].replace(")",'')
+        after_tomorrow_temp = ''.join(acquired.xpath('.//p[@class="op_weather4_twoicon_temp"]/text()')).strip('℃')
+        after_tomorrow_weather = ''.join(acquired.xpath('.//p[@class="op_weather4_twoicon_weath"]/text()')).strip()
+        after_tomorrow_wind = ''.join(acquired.xpath('.//p[@class="op_weather4_twoicon_wind"]/text()'))
+        after_tomorrow_aqi = ''.join(acquired.xpath('.//div[@class="op_weather4_twoicon_realtime_quality_wrap"]/span/text()')).strip() 
+        
+        three_day = weather_selector.xpath('.//div[@class="op_weather4_twoicon"]/a[4]')[0]
+        after_three_day_date = ''.join(three_day.xpath('.//p[@class="op_weather4_twoicon_date_day"]/text()'))
+        after_three_day_week = ''.join(three_day.xpath('.//p[@class="op_weather4_twoicon_date"]/text()')).strip() 
+        after_three_day_icon = ''.join(three_day.xpath('.//div[@class="op_weather4_twoicon_icon"]/@style')).split(";")[0].split("url(")[1].replace(")",'')
+        after_three_day_temp = ''.join(three_day.xpath('.//p[@class="op_weather4_twoicon_temp"]/text()')).strip('℃')
+        after_three_day_weather = ''.join(three_day.xpath('.//p[@class="op_weather4_twoicon_weath"]/text()')).strip() 
+        after_three_day_wind = ''.join(three_day.xpath('.//p[@class="op_weather4_twoicon_wind"]/text()'))
+        after_three_day_aqi = ''.join(three_day.xpath('.//div[@class="op_weather4_twoicon_realtime_quality_wrap"]/span/text()')).strip() 
+        
+        four_day = weather_selector.xpath('.//div[@class="op_weather4_twoicon"]/a[5]')[0]
+        after_four_day_date = ''.join(four_day.xpath('.//p[@class="op_weather4_twoicon_date_day"]/text()'))
+        after_four_day_week = ''.join(four_day.xpath('.//p[@class="op_weather4_twoicon_date"]/text()')).strip()
+        after_four_day_icon = ''.join(four_day.xpath('.//div[@class="op_weather4_twoicon_icon"]/@style')).split(";")[0].split("url(")[1].replace(")",'')
+        after_four_day_temp = ''.join(four_day.xpath('.//p[@class="op_weather4_twoicon_temp"]/text()')).strip('℃')
+        after_four_day_weather = ''.join(four_day.xpath('.//p[@class="op_weather4_twoicon_weath"]/text()')).strip() 
+        after_four_day_wind = ''.join(four_day.xpath('.//p[@class="op_weather4_twoicon_wind"]/text()'))
+        after_four_day_aqi = ''.join(four_day.xpath('.//div[@class="op_weather4_twoicon_realtime_quality_wrap"]/span/text()')).strip() 
+
+        weather_data = {
+                "code":"ok",
+                "city":city,
+                "data":{
+                    "0":{
+                        "date":today_date,
+                        "icon":today_icon,
+                        "current_temp":current_temp,
+                        "current_weather":current_weather,
+                        "temp":today_temp,
+                        "weather":today_weather,
+                        "wind":today_wind,
+                        "aqi":today_aqi 
+                        },
+                    "1":{
+                        "date":tomorrow_date,
+                        "icon":tomorrow_icon,
+                        "temp":tomorrow_temp,
+                        "weather":tomorrow_weather,
+                        "wind":tomorrow_wind,
+                        "aqi":tomorrow_aqi 
+                        },
+                    "2":{
+                        "date":after_tomorrow_date,
+                        "icon":after_tomorrow_icon,
+                        "temp":after_tomorrow_temp,
+                        "weather":after_tomorrow_weather,
+                        "wind":after_tomorrow_wind,
+                        "aqi":after_tomorrow_aqi 
+                        },
+                    "3":{
+                        "date":after_three_day_date,
+                        "icon":after_three_day_icon,
+                        "temp":after_three_day_temp,
+                        "weather":after_three_day_weather,
+                        "wind":after_three_day_wind,
+                        "aqi":after_three_day_aqi 
+                        },
+                    "4":{
+                        "date":after_four_day_date,
+                        "icon":after_four_day_icon,
+                        "temp":after_four_day_temp,
+                        "weather":after_four_day_weather,
+                        "wind":after_four_day_wind,
+                        "aqi":after_four_day_aqi 
+                        }
+                    }
+                }
+        if node.upper() == "NODEMCU":
+            year = datetime.datetime.now().year
+            nodemcu_weather_data = {
+                    "code":"200",
+                    "city":city,
+                    "data":{
+                    "0":{
+                        "weather":today_weather,
+                        "current_temp":current_temp,
+                        "aqi":today_aqi.split(",")[0], 
+                        "date":str(year)+"-"+today_date[:7].replace("月","-").replace("日","").strip(" "),
+                        "temp":today_temp.replace(' ~ ',' ')
+                        },
+                    "1":{
+                        "weather":tomorrow_weather,
+                        "date":tomorrow_date.replace("月","/").replace("日",""),
+                        "temp":tomorrow_temp.replace(' ~ ',' '),
+                        },
+                    "2":{
+                        "weather":after_tomorrow_weather,
+                        "date":after_tomorrow_date.replace("月","/").replace("日",""),
+                        "temp":after_tomorrow_temp.replace(' ~ ',' '),
+                        },
+                    "3":{
+                        "weather":after_three_day_weather,
+                        "date":after_three_day_date.replace("月","/").replace("日",""),
+                        "temp":after_three_day_temp.replace(' ~ ',' '),
+                        },
+                    }
+                }
+            for day in range(4):
+                nodemcu_weather_data["data"][str(day)]["weather_code"] = weather_data["data"][str(day)]["icon"].split('/')[-1].split('.')[0]
+            return nodemcu_weather_data 
+        print("get weather data from net")
+        return weather_data 
+
+def get_city_by_ip(ip_addr):
+    pass
+if __name__ == "__main__":
+    city = input("请输入城市名:")
+    w = GetWeather()
+    weather = w.get_weather_data(city, "esp8266")
+    print(weather)
---
+++ web.py
@@ -0,0 +1,43 @@
+#coding:utf-8
+import sys
+sys.path.append('../')
+
+from urllib.parse import urlencode
+from flask import Flask, request, jsonify
+from flask_caching import Cache
+from searchweather import GetWeather
+
+cache_config = {
+        "CACHE_TYPE":"redis",
+        "CACHE_REDIS_HOST":"172.27.0.14",
+        "CACHE_REDIS_PORT":"12015",
+        "CACHE_REDIS_DB":"",
+        "CACHE_REDIS_PASSWORD": "sUS7?io#hP%m6ODB"
+        }
+w = GetWeather()
+print("实例化")
+app = Flask(__name__)
+cache = Cache(app,config=cache_config)
+
+def cache_key():
+    args = request.args 
+    key = request.path+'?'+urlencode([(k,v.upper()) for k in sorted(args) for v in sorted(args.getlist(k))])
+    return key 
+
+@app.route("/weather/v1",methods=['GET'])
+@cache.cached(timeout=20*60, key_prefix=cache_key)
+def getweather():
+    _args = ["city","node"]
+    if request.method == "GET":
+        ip = request.remote_addr
+        # print("ip是{}".format(ip))
+        inspect = set(request.args.keys()).issubset(_args)
+        city, node = request.args.get('city', ''), request.args.get('node', '')
+        if node and node.upper() != "NODEMCU":
+            return jsonify({"code":"error","data":"node参数值不正确！node's value error！"})
+        weather_data = w.get_weather_data(city, node)
+        return jsonify(weather_data) if inspect else jsonify({"code":"error","data":"参数错误！Parameter error！"})
+    return jsonify({"code":"error","data":"错误的请求方式！Request method error！"})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8008)
