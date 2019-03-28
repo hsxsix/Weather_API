327efe0a89754e7f81445e28314729cb5a5957c0
hsxsix <hsxsix@gmail.com>
2019-03-28 11:01:13 +0800

    web base on flask

--- web.py
+++ web.py
@@ -6,6 +6,7 @@ from urllib.parse import urlencode
 from flask import Flask, request, jsonify
 from flask_caching import Cache
 from searchweather import GetWeather
+from modify import weather_modify
 
 cache_config = {
         "CACHE_TYPE":"redis",
@@ -14,8 +15,8 @@ cache_config = {
         "CACHE_REDIS_DB":"",
         "CACHE_REDIS_PASSWORD": "sUS7?io#hP%m6ODB"
         }
-w = GetWeather()
-print("实例化")
+weather = GetWeather()
+
 app = Flask(__name__)
 cache = Cache(app,config=cache_config)
 
@@ -32,11 +33,12 @@ def getweather():
         ip = request.remote_addr
         # print("ip是{}".format(ip))
         inspect = set(request.args.keys()).issubset(_args)
-        city, node = request.args.get('city', ''), request.args.get('node', '')
-        if node and node.upper() != "NODEMCU":
-            return jsonify({"code":"error","data":"node参数值不正确！node's value error！"})
-        weather_data = w.get_weather_data(city, node)
-        return jsonify(weather_data) if inspect else jsonify({"code":"error","data":"参数错误！Parameter error！"})
+        city, node = request.args.get('city', ''), request.args.get('node', 'normal')
+        weather_data = weather.weather_data(city)
+        result_weather = weather_modify.weather(weather_data, node)
+        
+        return jsonify(result_weather) if inspect else \
+                jsonify({"code":"error","data":"参数错误！Parameter error！"})
     return jsonify({"code":"error","data":"错误的请求方式！Request method error！"})
 
 if __name__ == "__main__":
